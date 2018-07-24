9491e077a0553389023934dd630b2c2177ca62f4
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-24 16:19:58 +0000

    Created and copied v9 from the original YelpCamp directory to this new branch

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-var express        = require('express'),
-    app            = express(),
-    bodyParser     = require('body-parser'),
-    mongoose       = require('mongoose'),
-    passport       = require('passport'),
-    LocalStrategy  = require('passport-local'),
-    methodOverride = require('method-override'),
+var express       = require('express'),
+    app           = express(),
+    bodyParser    = require('body-parser'),
+    mongoose      = require('mongoose'),
+    passport      = require('passport'),
+    LocalStrategy = require('passport-local'),
 // For The Modules
-    Campground     = require('./models/campground'),
-    Comment        = require('./models/comment'),
-    User           = require('./models/user'),
-    seedDB         = require('./seeds');
-
-// ===================================================    
-// unrelated to the YelpCamp Development -- including LESS for my CSS
-/*
-var lessMiddleware = require('less-middleware');
-
-app.use(lessMiddleware({
-    src      : __dirname + '/public',
-    compress : true
-}));
-*/
-// ===================================================
+    Campground    = require('./models/campground'),
+    Comment       = require('./models/comment'),
+    User          = require('./models/user'),
+    seedDB        = require('./seeds');
+    
 
 // For navigating through routes:
     var commentRoutes = require('./routes/comments'),
@@ -30,9 +18,9 @@ app.use(lessMiddleware({
     
     
 mongoose.Promise  = global.Promise;
-mongoose.connect('mongodb://localhost/yelp_camp_v10', {useMongoClient: true});
+mongoose.connect('mongodb://localhost/yelp_camp_v3', {useMongoClient: true})
 
-// seedDB();
+//seedDB();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -50,9 +38,6 @@ app.use(function(req, res, next){
 app.use(express.static(__dirname + '/public'))
 
 app.set("view engine", "ejs");
-
-// For Method Override (v10 Stuff):
-app.use(methodOverride('_method'));
     
     
 // ===================== Passport Configuration
--- campgrounds.js
+++ campgrounds.js
@@ -29,11 +29,8 @@ router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
-    };
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name, image: image, description: desc};
+    
     // Create a new campground & save it to the DB
     Campground.create(newCampground, function(err, newlyCreated){
         if (err) {
